feb424ba7d05e634d3824086bf86d8a090371ee2
Numan Safi <numan.safi@clear.bank>
2023-04-25 20:26:08 +0100

    add todo app

---
+++ TodoApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TodoApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, There!");
+        Console.WriteLine("Please Select one of the options below: ");
+        Console.WriteLine("[S]ee all Todo items");
+        Console.WriteLine("[A]dd a Todo item");
+        Console.WriteLine("[R]emove a Todo item");
+        Console.WriteLine("[E]xit");
+    }
+}
