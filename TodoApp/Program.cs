b2426aecdffb65cfbd99f5aa5b33725e97d291d9
Numan Safi <numan.safi@clear.bank>
2023-04-25 22:26:11 +0100

    add all options for users

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -10,5 +10,30 @@ internal class Program
         Console.WriteLine("[A]dd a Todo item");
         Console.WriteLine("[R]emove a Todo item");
         Console.WriteLine("[E]xit");
+
+        var userChoice = Console.ReadLine();
+        PrintSelectedOptions(userChoice);
+        Console.WriteLine(StringToInt(userChoice));
     }
+
+    private static bool IsLong(string input)
+    {
+        return input.Length > 10;
+    }
+
+    private static void PrintSelectedOptions(string selectedOptions)
+    {
+        Console.WriteLine($"Selected Options: {selectedOptions}");
+    }
+
+    private static int SumOfTwoNumbers(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    static int StringToInt(string value)
+    {
+        return int.Parse(value);
+    }
+
 }
