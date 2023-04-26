80d0829e493673229e6a2e942a1743fff1429ef0
Numan Safi <numan.safi@clear.bank>
2023-04-26 22:53:09 +0100

    added classes

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -4,36 +4,40 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        string? userChoice;
+
+        do
+        {
+            UserInterface.ShowMenu();
+            userChoice = Console.ReadLine().ToUpper();
+        } while (!Validation.ValidateUserChoice(userChoice));
+
+    }
+
+
+
+
+}
+
+class UserInterface
+{
+    public static void ShowMenu()
+    {
         Console.WriteLine("Hello, There!");
         Console.WriteLine("Please Select one of the options below: ");
         Console.WriteLine("[S]ee all Todo items");
         Console.WriteLine("[A]dd a Todo item");
         Console.WriteLine("[R]emove a Todo item");
         Console.WriteLine("[E]xit");
-
-        var userChoice = Console.ReadLine();
-        PrintSelectedOptions(userChoice);
-        Console.WriteLine(StringToInt(userChoice));
     }
+}
 
-    private static bool IsLong(string input)
+class Validation
+{
+    public static bool ValidateUserChoice(string userChoice)
     {
-        return input.Length > 10;
-    }
+        string[] menuButtons = { "S", "A", "R", "E" };
 
-    private static void PrintSelectedOptions(string selectedOptions)
-    {
-        Console.WriteLine($"Selected Options: {selectedOptions}");
+        return userChoice.Length == 1 && menuButtons.Contains(userChoice);
     }
-
-    private static int SumOfTwoNumbers(int num1, int num2)
-    {
-        return num1 + num2;
-    }
-
-    static int StringToInt(string value)
-    {
-        return int.Parse(value);
-    }
-
 }
