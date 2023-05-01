7d8eeed0e14af5011885f3e99c519e66b172e956
Numan Safi <numan.safi@clear.bank>
2023-05-01 22:42:03 +0100

    add new OOP project

---
+++ UnderstandingOOP/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace UnderstandingOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rectangle = new Rectangle(2, 5);
+            Console.WriteLine(rectangle.Width);
+            Console.WriteLine(rectangle.Height);
+
+            
+        }
+    }
+}
+
+class Rectangle
+{
+    public int Width { get; }
+    public int Height { get; }
+
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+}
