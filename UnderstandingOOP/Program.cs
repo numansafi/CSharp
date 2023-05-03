21fc9c7b07ae5fb782d62bf411a9d3e6c3cb0423
Numan Safi <numan.safi@clear.bank>
2023-05-03 22:12:05 +0100

    add classes

--- UnderstandingOOP/Program.cs
+++ UnderstandingOOP/Program.cs
@@ -5,14 +5,43 @@
         static void Main(string[] args)
         {
             var rectangle = new Rectangle(2, 5);
-            Console.WriteLine(rectangle.Width);
-            Console.WriteLine(rectangle.Height);
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.CalculateCircumference());
 
-            
+
+
         }
     }
 }
 
+class MedicalAppointment
+{
+    private string _patientName;
+    private DateTime _date;
+
+
+    public MedicalAppointment() : this ("Numan", 7)
+    {
+
+    }
+
+    public MedicalAppointment(string patientName, int daysFromNow)
+    {
+        _patientName = patientName;
+        _date = DateTime.Now.AddDays(daysFromNow);
+
+    }
+
+    public void Reschedule(DateTime date)
+    {
+        _date = date;
+    }
+
+    public void Reschedule(int month, int day)
+    {
+        _date = new DateTime(_date.Year, month, day);
+    }
+}
 class Rectangle
 {
     public int Width { get; }
@@ -22,5 +51,15 @@ class Rectangle
     {
         Width = width;
         Height = height;
+    }
+
+    public int CalculateCircumference()
+    {
+        return (2 * Width) + (2 * Height);
+    }
+
+    public int CalculateArea()
+    {
+        return Width * Height;
     }
 }
