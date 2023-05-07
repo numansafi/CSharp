8a089be2da27408a52f4a6562127008a6019e89b
Numan Safi <numan.safi@clear.bank>
2023-05-08 00:07:28 +0100

    add a dice class

--- src/2.OOP/DiceRollGame/Program.cs
+++ src/2.OOP/DiceRollGame/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace DiceRollGame
-{
-    internal class Program
+﻿namespace DiceRollGame;
+public class Program
+{ 
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
+}
+
+public class Dice
+{
+    private readonly Random _random;
+    private const int NumberOfSides = 6;
+
+    public Dice(Random random)
+    {
+        _random = random;
+    }
+
+    public int Roll() => _random.Next(1, NumberOfSides + 1);
 }
